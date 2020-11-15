6ea87a8b3b1befbc41017ee8f4bda700888dda39
Relativiteit <alejocain@gmail.com>
2020-11-15 21:56:11 +0100

    navigating websites and clicking

--- webscrapers/page_nav_click.py
+++ webscrapers/page_nav_click.py
@@ -1,13 +1,14 @@
 """ Project page_nav_click
     Created on 15-11-2020, 19:30
     @author Alejo Cain """
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import time
+
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
